c915b18947c044a6a5c50eb65b347fe025985c6b
tsurudak <tsurudak@users.noreply.github.com>
2016-09-25 20:06:54 +0200

    R code and plots for graphs 1 and 2

---
+++ plot1.R
@@ -0,0 +1,40 @@
+# set working directory for assignment
+setwd("/Users/KT/Documents/Coursera/4ExploratoryDataAx/assignment1")
+
+# # download and unzip files from the internet
+# # make variable to show where data is
+# url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# 
+# # download data
+# download.file(url, "zippedData.zip", method="curl")
+# 
+# #unzip downloaded data
+# unzip("zippedData.zip")
+# 
+# #list files in working directory
+# list.files()
+
+# ASSIGNMENT DATA --------
+dat <- read.table("household_power_consumption.txt", sep = ";", 
+                  header = T, na.strings = "?")
+
+# quick look at data
+str(dat)
+
+# convert date variable into date
+dat$Date <- as.Date(dat[,"Date"], format = "%d/%m/%Y")
+
+# take only data between 2007-02-01 and 2007-02-02 (inclusive)
+dat1 <- subset(dat, Date >= as.Date("01/02/2007",  format = "%d/%m/%Y") &
+                 Date <= as.Date("02/02/2007",  format = "%d/%m/%Y"))
+
+#write plot to png
+png('plot1.png', width = 480, height = 480,)
+
+# make plot
+with(dat1, hist(Global_active_power, col = "red",
+                xlab = "Global Active Power (kilowatts)",
+                main = "Global Active Power"))
+
+dev.off()
+
